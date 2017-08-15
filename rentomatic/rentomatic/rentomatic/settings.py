ec8bce5c8f3998e19bae341d20631673a1249545
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2017-08-16 00:49:15 +0800

    make test for StorageRoomListUseCase pass

--- rentomatic/rentomatic/rentomatic/settings.py
+++ rentomatic/rentomatic/rentomatic/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'src.domain',
+    'src.use_cases',
 ]
 
 MIDDLEWARE = [
--- rentomatic/rentomatic/tests/use_cases/test_storageroom_use_cases.py
+++ rentomatic/rentomatic/tests/use_cases/test_storageroom_use_cases.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
+from src.use_cases.storageroom_use_cases import StorageRoomListUseCase
+from src.domain.storageroom import StorageRoom
 
 
 class TestStorageRoomListUseCase(TestCase):
     
-    def domain_storagerooms():
-        storageroom_1 = models.StorageRoom(
+    def test_storage_room_list_use_case(self):        
+        storageroom_1 = StorageRoom(
             code='f853578c-fc0f-4e65-81b8-566c5dffa35a',
             size=215,
             price=39,
@@ -12,35 +14,12 @@ class TestStorageRoomListUseCase(TestCase):
             latitude='51.75436293',
         )
 
-        storageroom_2 = models.StorageRoom(
-            code='fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a',
-            size=405,
-            price=66,
-            longitude='0.18228006',
-            latitude='51.74640997',
-        )
-
-        storageroom_3 = models.StorageRoom(
-            code='913694c6-435a-4366-ba0d-da5334a611b2',
-            size=56,
-            price=60,
-            longitude='0.27891577',
-            latitude='51.45994069',
-        )
-
-        storageroom_4 = models.StorageRoom(
-            code='eed76e77-55c1-41ce-985d-ca49bf6c0585',
-            size=93,
-            price=48,
-            longitude='0.33894476',
-            latitude='51.39916678',
-        )
-
-        return [storageroom_1, storageroom_2, storageroom_3, storageroom_4]
-
-    def test_storage_room_list_use_case(self):
         storageroom_list_use_case = StorageRoomListUseCase()
 
         response = storageroom_list_use_case.execute()
 
-        assert response == domain_storagerooms
+        assert response[0].code == storageroom_1.code
+        assert response[0].size == storageroom_1.size
+        assert response[0].price == storageroom_1.price
+        assert response[0].longitude == storageroom_1.longitude
+        assert response[0].latitude == storageroom_1.latitude
