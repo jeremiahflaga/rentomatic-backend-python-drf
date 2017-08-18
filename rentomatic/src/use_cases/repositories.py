2a0ebf9165aa079dff3bfa0f69ebb9ee63fb18f8
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2017-08-19 03:22:52 +0800

    make repository failing test to pass

--- rentomatic/src/use_cases/repositories.py
+++ rentomatic/src/use_cases/repositories.py
@@ -1,13 +1,11 @@
 
 class StorageRoomRepository(object):
+
+    def __init__(self):
+        self.storagerooms = []
     
     def get_all(self):
-        storageroom1 = {
-            'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35a',
-            'size': 215,
-            'price': 39,
-            'longitude': '-0.09998975',
-            'latitude': '51.75436293',
-        }
+        return self.storagerooms
 
-        return [StorageRoom.from_dict(storageroom1)]
+    def add(self, storageroom):
+        self.storagerooms.append(storageroom)
